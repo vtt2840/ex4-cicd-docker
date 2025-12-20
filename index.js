bcf9ffd2580553e84891d439923d12acf7fd2b2a
vtt2840 <vuthutrang2840@gmail.com>
2025-12-20 18:08:59 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.get("/", async (req, res) => {
   res.status(200).send("Xin chào bạn");
 });
-// Start server
+
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
